bc3015662997fc6476edf6d21642a930a14fc998
tuan.nguyen <tuan.nguyen@jayeson.com.sg>
2017-02-16 11:02:10 +0800

    move authenticated function to separate into a controller

---
+++ src/main/java/com/mlaz/api/controller/AuthenticationController.java
@@ -0,0 +1,26 @@
+package com.mlaz.api.controller;
+
+import java.security.Principal;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableOAuth2Sso
+@RestController
+public class AuthenticationController extends WebSecurityConfigurerAdapter {
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login**", "/webjars/**").permitAll().anyRequest()
+				.authenticated().and().logout().logoutSuccessUrl("/").permitAll().and().csrf()
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	}
+}
